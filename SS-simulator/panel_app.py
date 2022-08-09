8c687b1b81e7ff2523083299c71d9132dce8e3ed
guillermo pc <slashprototype@gmail.com>
2022-08-09 17:24:30 -0500

    fixed errors

--- SS-simulator/panel_app.py
+++ SS-simulator/panel_app.py
@@ -102,7 +102,7 @@ class Application():
         
         # SPEED B
         def set_speed_plus_B():
-            self.rutina_B.set(self.rutina_B.get()+1)
+            self.speed_B.set(self.speed_B.get()+1)
             # print(self.rutina.get())
 
         def set_speed_minus_B():
