3dcc09dd3b94118281c8ac7f3b86275d05a19d01
guillermo pc <slashprototype@gmail.com>
2022-08-10 17:43:06 -0500

    Changes in the new class application

--- UR/classes/application/application.py
+++ UR/classes/application/application.py
@@ -1,6 +1,9 @@
+from ast import While
 from threading import Thread
 import sys
 import time
+from tracemalloc import stop
+from unittest import runner
 
 class Application ():
     def __init__(self, mqtt,robot,subscribe_topics,publish_topics,routines_path):
@@ -11,45 +14,50 @@ class Application ():
         self.routines_path = routines_path
         self.running = False
         
-        self.robot_thread = Thread(target=self.robot_loop)
+        self.robot_thread = Thread(target=self.robot_loop, args =(lambda : self.running, ),daemon=True)
         self.mqtt_thread = Thread(target=self.mqtt_loop)
         self.main_thread = Thread(target=self.main_loop)
         
         self.start_app()
+        self.main_loop()
 
     def start_app(self):
         self.running = True
         self.robot_thread.start()
+        self.mqtt_thread.start()
+
 
     def close_app(self):
-        self.running = False
-        pass
+
+        for i in range (5):
+            self.running = False
+            time.sleep(0.1)
+            self.robot_thread.join()
+            self.mqtt_thread.join()
+        print('clossing app')
+        sys.exit()
+
+
+# ---------------------THREADS AND CONTROL LOOP---------------------------
 
     def main_loop(self):
-        pass
+        while True:
+            try:
+                pass
+            except KeyboardInterrupt:            
+                self.close_app()
+                
 
     def mqtt_loop(self):
-        pass
+        while (self.running):
+            time.sleep(0.1)
+            print(self.mqtt)
     
-    def robot_loop(self):
-        while (True):
-            try:
-                time.sleep(1)
-                print(self.mqtt,self.robot,self.subscribe_topics,self.publish_topics,self.routines_path)
-                if KeyboardInterrupt == True:
-                    self.robot_thread.join(timeout=2)
-                    sys.exit()
-                    break
-                    self.running = False
-                    self.robot_thread.join()
-                    
-            except KeyboardInterrupt:
-                self.robot_thread.join(timeout=2)
-                sys.exit()
-                break
-                self.running = False
-                self.robot_thread.join()
-                sys.exit()
+    def robot_loop(self, stop):
+        while (self.running):
+            time.sleep(0.1)
+            print(self.robot)
+
 
 if __name__ == '__main__':
     app = Application('broker','UR3-A','subscrib','publish','path')
--- UR/classes/application/test.py
+++ UR/classes/application/test.py
@@ -1,15 +1,14 @@
 import time
 from threading import Thread
+import sys
 
-
+interrup = False
 
 def loop():
     while True:
-        try:
-            time.sleep(1)
-            print(1)
-        except KeyboardInterrupt:
-            th.join(timeout=1)
+        time.sleep(1)
+        print(1)
+        if interrup == True:
             break
 
 th = Thread(target=loop)
@@ -19,5 +18,7 @@ while True:
     try:
         time.sleep(0.5)
         print('main')
+
     except KeyboardInterrupt:
-        th.join(timeout=1)
+        interrup = True
+        th.join()
