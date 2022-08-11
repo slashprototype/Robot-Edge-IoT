318cd1c5b4ab494f45a3f1559909c1c32dbbf3d0
guillermo pc <slashprototype@gmail.com>
2022-08-11 11:48:56 -0500

    added visor sending job

--- UR/main.py
+++ UR/main.py
@@ -126,10 +126,17 @@ def control_loop(mqtt,robot,subscribe_topics,publish_topics,routines_path):
                                 robot_status = 7
                                 if status == 3:
                                     if target_id < targets_len-1:
-                                        target_id = target_id + 1
-                                        robot.sync_setpoint(targets,target_id)
-                                        robot.sync_program(start = 0)
-                                        stm_com = 1
+                                        
+                                        if target_id == 1 and ctrl_commad == 130:
+                                            mqtt.publish(publish_topics[17],2)
+                                            print('sending job')
+
+                                        else:
+                                            target_id = target_id + 1
+                                            robot.sync_setpoint(targets,target_id)
+                                            robot.sync_program(start = 0)
+                                            stm_com = 1
+
                                     else:
                                         print('routine complete, select another routine') 
                                         _resultwork = 170
