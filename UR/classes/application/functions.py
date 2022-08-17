c1964df3f9c30880ca01492533b7ade2f9e4cb3a
guillermo pc <slashprototype@gmail.com>
2022-08-17 11:01:45 -0500

    Some changes in app structure

---
+++ UR/classes/application/functions.py
@@ -0,0 +1,22 @@
+def get_gemma_type(gemma):
+    gemma_type = {
+        'D1':1,
+        'D2':1,
+        'D3':1,
+        'A1':2,
+        'A2':2,
+        'A3':2,
+        'A4':2,
+        'A5':2,
+        'A6':2,
+        'A7':2,
+        'F1':3,
+        'F2':3,
+        'F3':3,
+        'F4':3,
+        'F5':3,
+        'F6':3
+    }
+    return(gemma_type[gemma])
+
+    
