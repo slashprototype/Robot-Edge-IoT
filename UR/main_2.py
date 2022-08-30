fd6870bbc7f9fbe6e9cb8c58e6a261fb5c3451eb
guillermo pc <slashprototype@gmail.com>
2022-08-30 11:14:36 -0500

    cc

--- UR/main_2.py
+++
@@ -1,9 +0,0 @@
-import time
-from datetime import datetime
-import sys
-from classes.app.app import App
-from classes.mqtt.mqtt import Mqtt
-from classes.robotUR.robotUR import Robot
-
-app = App('broker','UR3-A','subscrib','publish','path')
-# def __init__(self, mqtt,robot,subscribe_topics,publish_topics,routines_path):
