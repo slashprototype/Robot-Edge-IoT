86a92e5e6ad2fd00a8b6340bf2694db3a09aca4e
guillermo pc <slashprototype@gmail.com>
2022-08-31 16:21:25 -0500

    xx

--- UR/classes/app/app.py
+++ UR/classes/app/app.py
@@ -93,7 +93,7 @@ class App ():
                         counter_1 = counter_1 + 1
                 
                 if self.robot_control_setup:
-                    if self.robot_status <= 6 and self.fsm_robot_control >= 10 and self.fsm_robot_control != 30 :
+                    if self.robot_status <= 6 and self.fsm_robot_control != 10 and self.fsm_robot_control != 30 :
                         self.fsm_robot_control = 30
 
                 
