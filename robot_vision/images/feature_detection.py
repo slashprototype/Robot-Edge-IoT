0991d7e2ed6b49e3fe9445e1b181221c8005d9b4
guillermo pc <slashprototype@gmail.com>
2022-09-02 15:50:55 -0500

    cc

--- robot_vision/images/feature_detection.py
+++ robot_vision/images/feature_detection.py
@@ -1,22 +1,21 @@
 import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as plt
-import sys
-sys.path.append('../images/')
-
 img1 = cv.imread('qr.jpg',cv.IMREAD_GRAYSCALE)          # queryImage
 img2 = cv.imread('capture.jpeg',cv.IMREAD_GRAYSCALE) # trainImage
-# Initiate ORB detector
-orb = cv.ORB_create()
-# find the keypoints and descriptors with ORB
-kp1, des1 = orb.detectAndCompute(img1,None)
-kp2, des2 = orb.detectAndCompute(img2,None)
-# create BFMatcher object
-bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
-# Match descriptors.
-matches = bf.match(des1,des2)
-# Sort them in the order of their distance.
-matches = sorted(matches, key = lambda x:x.distance)
-# Draw first 10 matches.
-img3 = cv.drawMatches(img1,kp1,img2,kp2,matches[:10],None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+# Initiate SIFT detector
+sift = cv.SIFT_create()
+# find the keypoints and descriptors with SIFT
+kp1, des1 = sift.detectAndCompute(img1,None)
+kp2, des2 = sift.detectAndCompute(img2,None)
+# BFMatcher with default params
+bf = cv.BFMatcher()
+matches = bf.knnMatch(des1,des2,k=2)
+# Apply ratio test
+good = []
+for m,n in matches:
+    if m.distance < 0.75*n.distance:
+        good.append([m])
+# cv.drawMatchesKnn expects list of lists as matches.
+img3 = cv.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 plt.imshow(img3),plt.show()
